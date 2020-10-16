9567b3a66e16a73483b839c91ad7fc834150396b
Hangry <hangrylydevour@gmail.com>
2020-10-16 14:57:29 +0800

    user model - able to create new user

--- models/user.py
+++ models/user.py
@@ -1,6 +1,16 @@
 from models.base_model import BaseModel
+from werkzeug.security import generate_password_hash
 import peewee as pw
 
 
 class User(BaseModel):
-    name = pw.CharField(unique=False)
+    username = pw.CharField()
+    email = pw.CharField(unique=True)
+    password = pw.TextField()
+
+    def validate(self):
+        duplicate_emails = User.get_or_none(User.email == self.email)
+        if duplicate_emails:
+            self.error.append('There is an existing account with this email address')
+        else:
+            self.password = hashed_password = generate_password_hash(self.password)
